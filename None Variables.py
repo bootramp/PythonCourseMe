cb1a13c4e872adf9a9bcf70a00261824dcb6fc78
bootramp <miladmahmoodian78@gmail.com>
2023-03-17 13:40:04 +0330

    Add Comment to what we use none

--- None Variables.py
+++ None Variables.py
@@ -1,3 +1,5 @@
+#Programmers often define their variables at the beginning of a program because placing them makes if easy to see the names of all the 
+#variables used by a chunk of code.
 variable_1 = None
 for i in range(1,5) :
     print("Gili" , variable_1)
